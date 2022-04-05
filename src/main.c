331e097192c360bad1b02d10161e07f0a56e6cf1
Marrek Nožka <nozka@spseol.cz>
2022-04-05 14:43:57 +0200

    funguje a vypisuje na UART

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include "milis.h"
 
 //#include "delay.h"
-//#include <stdio.h>
-//#include "uart1.h"
+#include "uart1.h"
+#include <stdio.h>
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -18,6 +18,15 @@
 #define BTN_PIN  GPIO_PIN_4
 #define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
 
+#define NCODER_CLK_PORT GPIOB
+#define NCODER_CLK_PIN GPIO_PIN_5
+#define NCODER_DATA_PORT GPIOB
+#define NCODER_DATA_PIN GPIO_PIN_4
+#define NCODER_SW_PORT GPIOB
+#define NCODER_SW_PIN GPIO_PIN_3
+#define NCODER_GET_CLK (GPIO_ReadInputPin(NCODER_CLK_PORT, NCODER_CLK_PIN) != RESET)
+#define NCODER_GET_DATA ( GPIO_ReadInputPin(NCODER_DATA_PORT, NCODER_DATA_PIN)!=RESET)
+#define NCODER_GET_SW ( GPIO_ReadInputPin(SW_PORT, NCODER_SW_PIN )==RESET)
 
 void setup(void)
 {
@@ -25,28 +34,59 @@ void setup(void)
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
 
+    GPIO_Init(NCODER_CLK_PORT, NCODER_CLK_PIN, GPIO_MODE_IN_FL_NO_IT);
+    GPIO_Init(NCODER_DATA_PORT, NCODER_DATA_PIN, GPIO_MODE_IN_FL_NO_IT);
+    GPIO_Init(NCODER_SW_PORT, NCODER_SW_PIN, GPIO_MODE_IN_PU_NO_IT);
+
     init_milis();
-    //init_uart1();
+    init_uart1();
 }
 
+int minule = 0;
+int8_t check_ncoder(void)
+{
+
+    if (minule == 0 && NCODER_GET_CLK == 1) {
+        // vzestupná hrana 
+        minule = 1;
+        if (NCODER_GET_DATA == 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    } 
+    else if (minule == 1 && NCODER_GET_CLK == 0) {
+        // sestupná hrana 
+        minule = 0;
+        if (NCODER_GET_DATA == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    return 0;
+}
 
 int main(void)
 {
     uint32_t time = 0;
-
+    uint32_t time1 = 0;
+    int16_t bagr = 0;
     setup();
 
     while (1) {
 
-        if (milis() - time > 333 && !BTN_PUSH) {
-            LED_REVERSE; 
+        if (milis() - time > 33) {
+            LED_REVERSE;
             time = milis();
-            //printf("%ld\n", time);
+            printf("\r  %5d     ", bagr);
         }
 
-        //LED_REVERSE; 
-        //delay_ms(333);
-        //printf("Funguje to!!!\n");
+        if (milis() - time1 > 1) {
+            time1 = milis();
+        }
+        bagr += check_ncoder();
+
     }
 }
 
